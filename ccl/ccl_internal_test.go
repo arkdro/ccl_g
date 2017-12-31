49ca3c4b5b8f87154ebc0e1088777f3dad957709
Arkadiy Drovosekov <arkdro@gmail.com>
2017-12-31 16:06:59 +0100

    add tests

---
+++ ccl/ccl_internal_test.go
@@ -0,0 +1,126 @@
+package ccl
+
+import (
+	"testing"
+)
+
+func Test_has_unmarked_background_below1(t *testing.T) {
+	color := 1
+	x := 1
+	y := 2
+	data := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	labels := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	actual := has_unmarked_background_below(color, x, y, &data, &labels)
+	expected := true
+	if actual != expected {
+		t.Error("has_unmarked_background_below 1 mismatch")
+	}
+}
+
+func Test_has_unmarked_background_below2(t *testing.T) {
+	color := 1
+	x := 1
+	y := 2
+	data := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 1, 3},
+	}
+	labels := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	actual := has_unmarked_background_below(color, x, y, &data, &labels)
+	expected := false
+	if actual != expected {
+		t.Error("has_unmarked_background_below 2 mismatch")
+	}
+}
+
+func Test_has_unmarked_background_below3(t *testing.T) {
+	color := 1
+	x := 1
+	y := 2
+	data := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	labels := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 1, 0},
+	}
+	actual := has_unmarked_background_below(color, x, y, &data, &labels)
+	expected := false
+	if actual != expected {
+		t.Error("has_unmarked_background_below 3 mismatch")
+	}
+}
+
+func Test_has_background_above1(t *testing.T) {
+	color := 1
+	x := 1
+	y := 2
+	data := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	dummy := []int{0, 0, 0}
+	actual := has_background_above(color, x, y, &data, &dummy)
+	expected := true
+	if actual != expected {
+		t.Error("has_background_above 1 mismatch")
+	}
+}
+
+func Test_has_background_above2(t *testing.T) {
+	color := 1
+	x := 1
+	y := 2
+	data := [][]int{
+		{1, 2, 3},
+		{1, 1, 3},
+		{1, 2, 3},
+	}
+	dummy := make([]int, 3)
+	actual := has_background_above(color, x, y, &data, &dummy)
+	expected := false
+	if actual != expected {
+		t.Error("has_background_above 2 mismatch")
+	}
+}
+
+func Test_has_background_above3(t *testing.T) {
+	color := 1
+	x := 1
+	y := 0
+	data := [][]int{
+		{1, 2, 3},
+		{1, 1, 3},
+		{1, 2, 3},
+	}
+	dummy := make([]int, 3)
+	actual := has_background_above(color, x, y, &data, &dummy)
+	expected := true
+	if actual != expected {
+		t.Error("has_background_above 3 mismatch")
+	}
+}
+
