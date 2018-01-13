1845ad989c60eee0a33d0cc41dfa61a7b1b06fee
Arkadiy Drovosekov <arkdro@gmail.com>
2018-01-13 23:59:17 +0100

    add test for ccl

--- ccl/ccl_test.go
+++ ccl/ccl_test.go
@@ -55,3 +55,42 @@ func Test_ccl(t *testing.T) {
 		t.Error("ccl labels mismatch")
 	}
 }
+
+func Test_ccl2(t *testing.T) {
+	width := 7
+	height := 4
+	color_range := 3
+	data := [][]int{
+		{1,1,0,2,2,2,2},
+		{1,1,2,0,2,2,2},
+		{0,1,1,2,2,1,0},
+		{0,0,0,2,2,2,0},
+	}
+	expected_labels0 := [][]int{
+		{0,0,1,0,0,0,0},
+		{0,0,0,1,0,0,0},
+		{2,0,0,0,0,0,3},
+		{2,2,2,0,0,0,3},
+	}
+	expected_labels1 := [][]int{
+		{1,1,0,0,0,0,0},
+		{1,1,0,0,0,0,0},
+		{0,1,1,0,0,2,0},
+		{0,0,0,0,0,0,0},
+	}
+	expected_labels2 := [][]int{
+		{0,0,0,1,1,1,1},
+		{0,0,1,0,1,1,1},
+		{0,0,0,1,1,0,0},
+		{0,0,0,1,1,1,0},
+	}
+	expected_labels := []*[][]int{
+		&expected_labels0,
+		&expected_labels1,
+		&expected_labels2,
+	}
+	labels := Ccl(width, height, color_range, &data)
+	if !reflect.DeepEqual(labels, expected_labels) {
+		t.Error("ccl 2 labels mismatch")
+	}
+}
