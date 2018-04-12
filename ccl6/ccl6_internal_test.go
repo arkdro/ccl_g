2e44ad3121fe5a8f1db964abdc0f0aeb51472182
Arkadiy Drovosekov <arkdro@gmail.com>
2018-04-12 23:01:59 +0200

    fix tests

--- ccl6/ccl6_internal_test.go
+++ ccl6/ccl6_internal_test.go
@@ -8,234 +8,133 @@ import (
 	"testing"
 )
 
-func Test_get_left_point1(t *testing.T) {
-	color := color.Color(1)
+func Test_get_left_coordinate1(t *testing.T) {
 	x := 0
 	y := 2
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	_, status := get_left_point(color, x, y, &data)
+	_, status := get_left_coordinate(x, y)
 	expected := false
 	if status != expected {
-		t.Error("get_left_point 1 mismatch")
+		t.Error("get_left_coordinate 1 mismatch")
 	}
 }
 
-func Test_get_left_point2(t *testing.T) {
-	color := color.Color(1)
+func Test_get_left_coordinate2(t *testing.T) {
 	x := 1
 	y := 2
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	actual, status := get_left_point(color, x, y, &data)
+	actual, status := get_left_coordinate(x, y)
 	expected := point.Point{0, 2}
 	if status != true || actual != expected {
-		t.Error("get_left_point 2 mismatch")
+		t.Error("get_left_coordinate 2 mismatch")
 	}
 }
 
-func Test_get_left_point3(t *testing.T) {
-	color := color.Color(1)
+func Test_get_left_coordinate3(t *testing.T) {
 	x := 1
 	y := -1
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	_, status := get_left_point(color, x, y, &data)
+	_, status := get_left_coordinate(x, y)
 	expected := false
 	if status != expected {
-		t.Error("get_left_point 3 mismatch")
+		t.Error("get_left_coordinate 3 mismatch")
 	}
 }
 
-func Test_get_left_point4(t *testing.T) {
-	color := color.Color(4)
-	x := 1
-	y := 2
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	_, status := get_left_point(color, x, y, &data)
-	expected := false
-	if status != expected {
-		t.Error("get_left_point 4 mismatch")
-	}
-}
-
-func Test_get_upper_left_point1(t *testing.T) {
-	color := color.Color(4)
+func Test_get_upper_left_coordinate1(t *testing.T) {
 	x := 1
 	y := 0
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	_, status := get_upper_left_point(color, x, y, &data)
+	_, status := get_upper_left_coordinate(x, y)
 	expected := false
 	if status != expected {
-		t.Error("get_upper_left_point 1 mismatch")
+		t.Error("get_upper_left_coordinate 1 mismatch")
 	}
 }
 
-func Test_get_upper_left_point2(t *testing.T) {
-	color := color.Color(4)
+func Test_get_upper_left_coordinate2(t *testing.T) {
 	x := 0
 	y := 1
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	_, status := get_upper_left_point(color, x, y, &data)
+	_, status := get_upper_left_coordinate(x, y)
 	expected := false
 	if status != expected {
-		t.Error("get_upper_left_point 2 mismatch")
+		t.Error("get_upper_left_coordinate 2 mismatch")
 	}
 }
 
-func Test_get_upper_left_point3(t *testing.T) {
-	color := color.Color(4)
+func Test_get_upper_left_coordinate3(t *testing.T) {
 	x := 1
 	y := 1
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	pt, status := get_upper_left_point(color, x, y, &data)
+	pt, status := get_upper_left_coordinate(x, y)
 	expected := point.Point{0, 0}
 	if status != true || pt != expected {
-		t.Error("get_upper_left_point 3 mismatch")
+		t.Error("get_upper_left_coordinate 3 mismatch")
 	}
 }
 
-func Test_get_upper_left_point4(t *testing.T) {
-	color := color.Color(4)
+func Test_get_upper_left_coordinate4(t *testing.T) {
 	x := 1
 	y := 2
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
-	pt, status := get_upper_left_point(color, x, y, &data)
+	pt, status := get_upper_left_coordinate(x, y)
 	expected := point.Point{1, 1}
 	if status != true || pt != expected {
-		t.Error("get_upper_left_point 4 mismatch")
+		t.Error("get_upper_left_coordinate 4 mismatch")
 	}
 }
 
-func Test_get_upper_right_point1(t *testing.T) {
-	color := color.Color(4)
+func Test_get_upper_right_coordinate1(t *testing.T) {
 	x := 1
 	y := 0
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
 	width := 3
-	_, status := get_upper_right_point(width, color, x, y, &data)
+	_, status := get_upper_right_coordinate(width, x, y)
 	expected := false
 	if status != expected {
-		t.Error("get_upper_right_point 1 mismatch")
+		t.Error("get_upper_right_coordinate 1 mismatch")
 	}
 }
 
-func Test_get_upper_right_point2(t *testing.T) {
-	color := color.Color(4)
+func Test_get_upper_right_coordinate2(t *testing.T) {
 	x := 1
 	y := 1
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
 	width := 3
-	pt, status := get_upper_right_point(width, color, x, y, &data)
+	pt, status := get_upper_right_coordinate(width, x, y)
 	expected := point.Point{1, 0}
 	if status != true || pt != expected {
-		t.Error("get_upper_right_point 2 mismatch")
+		t.Error("get_upper_right_coordinate 2 mismatch")
 	}
 }
 
-func Test_get_upper_right_point3(t *testing.T) {
-	color := color.Color(3)
+func Test_get_upper_right_coordinate3(t *testing.T) {
 	x := 1
 	y := 2
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
 	width := 3
-	pt, status := get_upper_right_point(width, color, x, y, &data)
+	pt, status := get_upper_right_coordinate(width, x, y)
 	expected := point.Point{2, 1}
 	if status != true || pt != expected {
-		t.Error("get_upper_right_point 3 mismatch")
+		t.Error("get_upper_right_coordinate 3 mismatch")
 	}
 }
 
-func Test_get_upper_right_point4(t *testing.T) {
-	color := color.Color(3)
+func Test_get_upper_right_coordinate4(t *testing.T) {
 	x := 2
 	y := 2
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
 	width := 3
-	_, status := get_upper_right_point(width, color, x, y, &data)
+	_, status := get_upper_right_coordinate(width, x, y)
 	expected := false
 	if status != expected {
-		t.Error("get_upper_right_point 4 mismatch")
+		t.Error("get_upper_right_coordinate 4 mismatch")
 	}
 }
 
-func Test_get_upper_right_point5(t *testing.T) {
-	color := color.Color(3)
+func Test_get_upper_right_coordinate5(t *testing.T) {
 	x := 2
 	y := 1
-	data := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-	}
 	width := 3
-	pt, status := get_upper_right_point(width, color, x, y, &data)
+	pt, status := get_upper_right_coordinate(width, x, y)
 	expected := point.Point{2, 0}
 	if status != true || pt != expected {
-		t.Error("get_upper_right_point 5 mismatch")
+		t.Error("get_upper_right_coordinate 5 mismatch")
 	}
 }
 
 func Test_same_color_neigbours1(t *testing.T) {
-	color := 3
+	color := 1
 	x := 0
 	y := 1
 	data := [][]int{
@@ -253,18 +152,17 @@ func Test_same_color_neigbours1(t *testing.T) {
 }
 
 func Test_same_color_neigbours2(t *testing.T) {
-	color := 3
+	color := 1
 	x := 1
 	y := 1
 	data := [][]int{
-		{1, 2, 3},
+		{1, 1, 3},
 		{1, 2, 3},
 		{1, 2, 3},
 		{1, 2, 3},
 	}
 	width := 3
 	actual := same_color_neigbours(width, color, x, y, &data)
-	t.Log(actual)
 	expected1 := point.Point{0, 0}
 	expected2 := point.Point{1, 0}
 	expected0 := point.Point{0, 1}
