60afc524b6c62ef8ccaa7d18edc56ccb9c6c902a
Arkadiy Drovosekov <arkdro@gmail.com>
2018-04-15 15:56:18 +0200

    make test shorter

--- ccl6/ccl6_test.go
+++ ccl6/ccl6_test.go
@@ -47,20 +47,14 @@ func Test_ccl1(t *testing.T) {
 func Test_ccl2(t *testing.T) {
 	width := 7
 	height := 4
-	color_range := 2
+	color_range := 1
 	data := [][]int{
-		{ 0,0,1,0,0,0,0},
-		{0,0,0,0,1,0,1},
-		{ 0,0,1,1,1,0,1},
-		{1,1,1,1,0,1,0},
+		{ 7,7,0,7,7,7,7},
+		{7,7,7,7,0,7,0},
+		{ 7,7,0,0,0,7,0},
+		{0,0,0,0,7,0,7},
 	}
 	expected_labels0 := [][]int{
-		{1,1,0,1,1,1,1},
-		{1,1,1,1,0,1,0},
-		{1,1,0,0,0,1,0},
-		{0,0,0,0,3,0,1},
-	}
-	expected_labels1 := [][]int{
 		{0,0,1,0,0,0,0},
 		{0,0,0,0,2,0,3},
 		{0,0,2,2,2,0,3},
@@ -68,12 +62,10 @@ func Test_ccl2(t *testing.T) {
 	}
 	expected_labels := []*[][]int{
 		&expected_labels0,
-		&expected_labels1,
 	}
 	labels := Ccl(width, height, color_range, &data)
 	if !reflect.DeepEqual(labels, expected_labels) {
 		t.Error("ccl 2, labels mismatch")
 		t.Error("ccl 2, labels 0: ", labels[0])
-		t.Error("ccl 2, labels 1: ", labels[1])
 	}
 }
